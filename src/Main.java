ca8404e9744e412d09ed1e8d0f66773f541ea5e2
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-06-03 00:29:38 +0300

    add three class which wiil be created and serializated by users and add class which will be shown us everything serialization objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-       
+    public static void main(String[] args) throws IOException {
+        File file = new File("dog.dat");
+        Dog dog = new Dog("dog", 4);
+        Cat cat = new Cat("cat", 61);
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+        try {
+            oos.writeObject(dog);
+            oos.writeObject(cat);
+            oos.writeObject(cat);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            oos.close();
+        }
+        readObjectInFile(file);
+    }
+
+    /**
+     * @param file - file path;
+     * @throws IOException - the exception who may be throw because of making mistake;
+     */
+    public static void readObjectInFile(File file) throws IOException {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        try {
+            Animal obj = null;
+            while ((obj = (Animal) ois.readObject()) != null) {
+                System.out.println(obj.getAge());
+                System.out.println(obj.getName());
+            }
+        } catch (Exception e) {
+            e.fillInStackTrace();
+        } finally {
+            ois.close();
+        }
     }
 }
