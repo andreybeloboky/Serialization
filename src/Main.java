f37dce051c7dd3a5e776e3eb0f766ef6c20ab36f
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-06-29 10:00:47 +0300

    edit write method replace to separate method and using try-with-resourcs

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,30 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("dog.dat");
         Dog dog = new Dog("dog", 4);
         Cat cat = new Cat("cat", 61);
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-        try {
-            oos.writeObject(dog);
-            oos.writeObject(cat);
-            oos.writeObject(cat);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } finally {
-            oos.close();
-        }
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(dog);
+        animals.add(cat);
+        writeObjectInFile(file, animals);
         readObjectInFile(file);
     }
 
-    /**
-     * @param file - file path;
-     * @throws IOException - the exception who may be throw because of making mistake;
-     */
+    public static void writeObjectInFile(File file, ArrayList<Animal> animals) throws IOException { // the better use collections than arrays;
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));) {
+            for (Animal value : animals) {
+                oos.writeObject(value);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void readObjectInFile(File file) throws IOException {
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
-        try {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
             Animal obj = null;
             while ((obj = (Animal) ois.readObject()) != null) {
                 System.out.println(obj.getAge());
@@ -32,8 +32,6 @@ public class Main {
             }
         } catch (Exception e) {
             e.fillInStackTrace();
-        } finally {
-            ois.close();
         }
     }
 }
